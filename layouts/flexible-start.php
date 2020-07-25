fe68bf623d841c6c3b7cb271332af284768fb5fc
Tim Wu <tim@longevitygraphics.com>
2020-07-25 10:45:46 -0700

    add acf and product cpt

--- layouts/flexible-start.php
+++ layouts/flexible-start.php
@@ -5,12 +5,8 @@
     $background_color = $general_settings["background_color"];
     $padding_top = $general_settings["padding_top"];
     $padding_bottom = $general_settings["padding_bottom"];
-    $padding_left = $general_settings["padding_left"];
-    $padding_right = $general_settings["padding_right"];
     $margin_top = $general_settings["margin_top"];
     $margin_bottom = $general_settings["margin_bottom"];
-    $margin_left = $general_settings["margin_left"];
-    $margin_right = $general_settings["margin_right"];
     $custom_id = $general_settings["custom_id"];
     $custom_classes =  $general_settings["custom_classes"];
 
@@ -20,27 +16,15 @@
     if($padding_bottom){
         $pb_val = $padding_bottom['value'];
     } 
-    if($padding_left){
-        $pl_val = $padding_left['value'];
-    } 
-    if($padding_right){
-        $pr_val = $padding_right['value'];
-    } 
     if($margin_top){
         $mt_val = $margin_top['value'];
     }
     if($margin_bottom){
         $mb_val = $margin_bottom['value'];
     }
-    if($margin_left){
-        $ml_val = $margin_left['value'];
-    }
-    if($margin_right){
-        $mr_val = $margin_right['value'];
-    }
+    print_r($padding_top);
 ?>
 <section 
 <?php  if($custom_id) : ?>id="<?php echo $custom_id?>"<?php endif; ?>
-       <?php if($background_image) :?> style="background-image:url('<?php echo $background_image["url"]; ?>')"<?php endif;?>
-       class="hc-section flexible-content-start <?php if($background_color) : ?>bg-<?php echo $background_color['value']; ?> <?php endif; ?><?php echo $custom_classes . " " . $pt_val . " " . $pb_val . " " . $pl_val . " " .  $pr_val ?> <?php echo $mt_val . " " . $mb_val . " " . $ml_val . " " . $mr_val; ?>"> <!-- end of section -->
+       class="hc-section flexible-content-start <?php if($background_color) : ?>bg-<?php echo $background_color['value']; ?> <?php endif; ?><?php echo $custom_classes . " " . $pt_val . " " .  $pb_val ?> <?php echo $mt_val . " " . $mb_val; ?>"> <!-- end of section -->
        <div class="<?php echo $container['value']; ?>">
--- page.php
+++ page.php
@@ -9,30 +9,19 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package cedirectory
+ * @package Hip_Creations
  */
 
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+			<?php if(have_rows("flexible_layout")) : ?>
+				<?php while(have_rows("flexible_layout")) : the_row(); ?>
+					<?php get_template_part("layouts/flexible-layout"); ?>
+				<?php endwhile ?>
+			<?php endif?>
+		</main><!-- #main -->
+	</div><!-- #primary -->
 <?php
-get_sidebar();
 get_footer();
