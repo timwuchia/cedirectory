98639a42cc1159712137f81d0b7c3a9e17709093
Tim Wu <tim@longevitygraphics.com>
2020-08-01 14:16:36 -0700

    make changes to the header

--- template-parts/header/nav-top.php
+++ template-parts/header/nav-top.php
@@ -1,15 +1,16 @@
-<div class='nav-top bg-secondary px-3 py-2'>
-    <div class='nav-top-content'>
-        <div class='nav-top-wrapper d-flex  justify-content-end'>
+<div class='nav-top bg-secondary px-3 py-2 px-lg-0 py-lg-0 pt-lg-3 d-flex justify-content-end'>
+        <div class='nav-top-wrapper d-flex justify-content-end'>
             <?php 
                 $phone = get_field('ce_phone', 'options');
                 if($phone) :
             ?>
             
-            <a class='mr-3' href="tel:<?php echo $phone; ?>"><?php echo $phone; ?></a>
+            <a class='header-phone' href="tel:<?php echo $phone; ?>"><span class='d-none d-md-inline-block'>Call Us: </span><?php echo $phone; ?></a>
             <?php endif;?>
             <?php if(have_rows('ce_social_media', 'options')) : ?>
+            
             <div class='header-social-media'>
+                <span class='d-none d-md-inline-block'>Follow Us: </span>
                 <?php while(have_rows('ce_social_media', 'options')) : the_row(); ?>
                 <?php
                     $link = get_sub_field('link','options');
@@ -21,5 +22,4 @@
                 
             <?php endif;?>
         </div>
-    </div>
 </div>
