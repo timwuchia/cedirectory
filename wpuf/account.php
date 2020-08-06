f26eff6b6a7c481473ed4f514a3309f669e2aad7
Tim Wu <tim@longevitygraphics.com>
2020-08-06 11:10:43 -0700

    add user fields

--- wpuf/account.php
+++ wpuf/account.php
@@ -1,43 +1,45 @@
-<?php get_template_part('template-parts/account/profile-banner'); ?>
-<div class="wpuf-dashboard-container container">
-    <nav class="wpuf-dashboard-navigation">
-        <ul>
-            <?php
-                if ( is_user_logged_in() ) {
-                    foreach ( $sections as $section ) {
-                        if ( 'subscription' == $section['slug'] ) {
-                            if ( 'off' == wpuf_get_option( 'show_subscriptions', 'wpuf_my_account', 'on' ) || 'on' != wpuf_get_option( 'enable_payment', 'wpuf_payment', 'on' ) ) {
-                                continue;
+<div class="wpuf-dashboard-container">
+    <div class='profile-banner'>
+        <?php get_template_part('template-parts/account/profile-banner'); ?>
+        <nav class="wpuf-dashboard-navigation">
+            <ul>
+                <?php
+                    if ( is_user_logged_in() ) {
+                        foreach ( $sections as $section ) {
+                            if ( 'subscription' == $section['slug'] ) {
+                                if ( 'off' == wpuf_get_option( 'show_subscriptions', 'wpuf_my_account', 'on' ) || 'on' != wpuf_get_option( 'enable_payment', 'wpuf_payment', 'on' ) ) {
+                                    continue;
+                                }
                             }
-                        }
 
-                        if ( 'billing-address' == $section['slug'] ) {
-                            if ( 'off' == wpuf_get_option( 'show_billing_address', 'wpuf_my_account', 'on' ) || 'on' != wpuf_get_option( 'enable_payment', 'wpuf_payment', 'on' ) ) {
-                                continue;
+                            if ( 'billing-address' == $section['slug'] ) {
+                                if ( 'off' == wpuf_get_option( 'show_billing_address', 'wpuf_my_account', 'on' ) || 'on' != wpuf_get_option( 'enable_payment', 'wpuf_payment', 'on' ) ) {
+                                    continue;
+                                }
                             }
+
+                            $default_active_tab = wpuf_get_option( 'account_page_active_tab', 'wpuf_my_account', 'dashboard' );
+                            $active_tab         = false;
+
+                            if ( ( isset( $_GET['section'] ) && $_GET['section'] == $section['slug'] ) || ( !isset( $_GET['section'] ) && $default_active_tab == $section['slug'] ) ) {
+                                $active_tab = true;
+                            }
+
+                            $active = $active_tab ? $section['slug'] . ' active' : $section['slug'];
+                            echo sprintf(
+                                '<li class="wpuf-menu-item %s"><a href="%s">%s</a></li>',
+                                esc_attr( $active ),
+                                esc_attr( add_query_arg( [ 'section' => $section['slug'] ], get_permalink() ) ),
+                                esc_attr( $section['label'] )
+                            );
                         }
-
-                        $default_active_tab = wpuf_get_option( 'account_page_active_tab', 'wpuf_my_account', 'dashboard' );
-                        $active_tab         = false;
-
-                        if ( ( isset( $_GET['section'] ) && $_GET['section'] == $section['slug'] ) || ( !isset( $_GET['section'] ) && $default_active_tab == $section['slug'] ) ) {
-                            $active_tab = true;
-                        }
-
-                        $active = $active_tab ? $section['slug'] . ' active' : $section['slug'];
-                        echo sprintf(
-                            '<li class="wpuf-menu-item %s"><a href="%s">%s</a></li>',
-                            esc_attr( $active ),
-                            esc_attr( add_query_arg( [ 'section' => $section['slug'] ], get_permalink() ) ),
-                            esc_attr( $section['label'] )
-                         );
                     }
-                }
-            ?>
-        </ul>
-    </nav>
+                ?>
+            </ul>
+        </nav>
+    </div>
 
-    <div class="wpuf-dashboard-content <?php echo ( !empty( $current_section ) ) ? esc_attr( $current_section['slug'] ) : ''; ?>">
+    <div class="container wpuf-dashboard-content <?php echo ( !empty( $current_section ) ) ? esc_attr( $current_section['slug'] ) : ''; ?>">
         <?php
             if ( !empty( $current_section ) && is_user_logged_in() ) {
                 do_action( "wpuf_account_content_{$current_section['slug']}", $sections, $current_section );
