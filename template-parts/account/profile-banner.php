c8a2a5e0951fbdea19aa8e22ac30b4deb5051b02
Tim Wu <tim@longevitygraphics.com>
2020-08-11 18:51:05 -0700

    fix profile image not showing

--- template-parts/account/profile-banner.php
+++ template-parts/account/profile-banner.php
@@ -3,16 +3,14 @@
 // global $current_user;
 $current_user_id = get_current_user_id();
 $current_user_acf_params = 'user_' . $current_user_id;
-$profile_banner = get_field('profile_banner', $current_user_acf_params);
-$profile_image = get_field('profile_image', $current_user_acf_params);
 
 if(function_exists('get_field')){
+    $profile_banner = get_field('profile_banner', $current_user_acf_params);
+    $profile_image = get_field('profile_image', $current_user_acf_params);
     if($profile_banner){
-        echo wp_get_attachment_image($profile_banner['id'], 'full');
+        echo wp_get_attachment_image($profile_banner, 'full');
 	};
 	if($profile_image){
-        echo wp_get_attachment_image($profile_image['id'], 'medium');
+        echo wp_get_attachment_image($profile_image, 'medium');
     };
 }
-
-?>
