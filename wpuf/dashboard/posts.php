d8dedb8bab770887a95a1e6c4dc920636501870f
neblanes <32908332+neblanes@users.noreply.github.com>
2020-08-15 20:44:14 -0700

    styled the Dashboard Blog/ Post Page ( finished )

--- wpuf/dashboard/posts.php
+++ wpuf/dashboard/posts.php
@@ -8,9 +8,13 @@ if ( is_user_logged_in() ):
     $author_posts = new WP_Query($author_query);?>
     <div class='current-user-posts d-flex flex-wrap'>
         <?php while($author_posts->have_posts()) : $author_posts->the_post(); ?>
-        <div class='post'>
+        <div class='post post-container col-12 col-md-6 col-lg-4'>
             <?php the_post_thumbnail('medium');?>
+            <div class='post-title'>
+            <h2>
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+            </h2>
+            </div>
         </div>
         <?php 
             endwhile; 
