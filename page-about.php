8a0fffbf8d75500c4305c3bea5cc8ff612d4d9b6
neblanes <32908332+neblanes@users.noreply.github.com>
2020-08-16 16:38:25 -0700

    Styled the About Page ( finished )

--- page-about.php
+++ page-about.php
@@ -11,47 +11,84 @@ get_header();
 	
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+<div class='about-container'>
 		<?php while ( have_posts() ) :the_post(); ?>
 
     <?php 
 			if(function_exists("get_field")){
 
 
-					if(get_field("about_us_banner_image", "option") || get_field("about_us_content_description", "option") || get_field("about_us_layer_one_image", "option") ) {
-			echo '<div class="about-image-banner custom-container">';
+					if(get_field("about_us_banner_image", "option") || get_field("about_us_content_description", "option") || get_field("about_us_first_content_title", "option") || get_field("about_us_second_content_title", "option") || get_field("about_us_layer_one_image", "option") ) {
+
+
+			echo '<div class="about-image-banner-container">';
 					echo wp_get_attachment_image( get_field('about_us_banner_image', "option"), 'full' );
 			echo '</div>';
-			
-			echo '<div class="about-layer-one ">';
-					echo the_field("about_us_content_description_one", "option");
+
+	echo'<div class="about-content-container">';
+		
+		echo'<div class="about-content-one">';
+
+			echo '<div class="about-layer-one layer ">';
+				echo '<div class="description">';
+					echo '<h2> hello';
+						echo the_field("about_us_first_content_title", "option");
+					echo '</h2>';
+					echo '<p>';
+						echo the_field("about_us_content_description_one", "option");
+					echo '</p>';
+				echo '</div>';
+					
 						echo wp_get_attachment_image( get_field('about_us_layer_one_image', "option"), 'full' );
 			echo '</div>';
 
-			echo '<div class="about-layer-two">';
-							echo the_field("about_us_content_description_two", "option");
+			echo '<div class="about-layer-two layer">';
+							
 						echo wp_get_attachment_image( get_field('about_us_layer_two_image', "option"), 'full' );
+					echo '<div class="description">';
+						echo '<h2>';
+						echo the_field("about_us_second_content_title", "option");
+						echo '</h2>';
+						echo '<p>';
+						echo the_field("about_us_content_description_two", "option");
+						echo '</p>';
+					echo '</div>';
 			echo '</div>';
+		echo '</div>';
 
-			echo '<div class="about-layer-three">';
-						echo '<div class="layer-three-images">';
+			echo '<div class="about-layer-three layer">';
+					echo '<div class="layer-three-images">';
 						echo wp_get_attachment_image( get_field('image_one', "option"), 'full' );
 						echo wp_get_attachment_image( get_field('image_two', "option"), 'full' );
 						echo wp_get_attachment_image( get_field('image_three', "option"), 'full' );
-						echo '</div>';
+					echo '</div>';
 
-						echo '<div class="descriptions">';
-						echo the_field("layer_three_description_one", "option");
-						echo the_field("layer_three_description_two", "option");
-						echo the_field("layer_three_description_three", "option");
-						echo '</div>';
+					echo '<div class="btm-description description layer">';
+					echo '<span>';
+							echo '<p>';
+								echo the_field("layer_three_description_one", "option");
+							echo '</p>';
+					echo '</span>';
+					echo '<span>';
+							echo '<p>';
+								echo the_field("layer_three_description_two", "option");
+							echo '</p>';
+					echo '</span>';
+						echo '<span>';
+							echo '<p>';
+								echo the_field("layer_three_description_three", "option");
+							echo '</p>';
+					echo '</span>';
+					echo '</div>';
 			echo '</div>';
 
-
+echo '</div>';
+// end of about-content-container
 				}
 				}
      ?>
-   
+</div>
+<!-- about-container -->
     	<?php endwhile; // End of the loop.?>
 			</main><!-- #main -->
 	</div><!-- #primary -->
