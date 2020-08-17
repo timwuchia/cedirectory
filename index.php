1857b2622b0716f0caed53301de224f498ae03bb
neblanes <32908332+neblanes@users.noreply.github.com>
2020-08-17 00:45:40 -0700

    Styled the Main Blog Page ( finished )

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-<div class=' blog-container d-flex flex-wrap'> 
+
 		<?php
 			$args = array(
 				'numberposts' => 1,
@@ -39,61 +39,44 @@ get_header();
 
 	?>
 
-<div class = 'blog col-12 col-md-6 col-lg-4'>
-	<?php
-	foreach( $recent_posts as $recent ){	
-			
-			echo '<div class = "latest-blog-container">';
-			echo get_the_post_thumbnail($recent['ID'],'featured-large');
-			echo  '<a href="' . get_permalink() . '">';
-			echo "<div class ='latest-blog-title'>";
-			echo '<h1>';
-			echo $recent['post_title'];
-			echo '</h1>';
-			echo "<h2>Recent Blog</h2>";
-			echo "</div>";
-			echo "</a>";
-			echo '</div>';//end of latest-blog-container
 
-		wp_reset_query();
 
-		}
-
-		?>
-
+<div class="blog-page-container">
 	
-<div class="">
+
 			<?php
 			if ( have_posts() ) :
 
 				while ( have_posts() ) :
-						echo "<div class = 'blog-wrapper' >";
+					echo "<div class = 'blog-container' >";
 						echo the_post();
-						echo "<div class = 'blog-images' >";
-						echo '<a class="blog-img-sec" href="';
+					echo '<a href="';
 						echo the_permalink($post->ID);
 						echo '">';
 						echo the_post_thumbnail('work-img');
-						echo '<h2>';
-						echo get_the_title();
-						echo '</h2>';
-						echo '</a>';
-						echo "</div>";// end of blog-images
-						echo '<br>';
+					
+
+	
 						echo '<div class = "blog-content" >';
-						echo '<h4>';
-						echo get_the_date();
-						echo '</h4>';
-						echo '<p>';
-						echo the_excerpt();
-						echo '</p>';
+						
+							echo '<h3>';
+								echo get_the_title();
+							echo '</h3>';
+									echo '<p>';
+								echo get_the_date();
+							echo '</p>';
+						
+								echo '</a>';
+					
+					
 						echo '</div>';
+
 						echo "</div>";//end of blog-wrapper
 			endwhile; 
 			endif;
 			?>
-	</div><!--end of blog container	-->
 </div>
+
 
 		</main> 
 	</div>
