99ea87f0198c8ecb88a7515e8ea96ae7ceef441b
neblanes <32908332+neblanes@users.noreply.github.com>
2020-08-17 21:48:32 -0700

    Styled About Page ( finished ) , Styled Resource Page ( finished ) , Styled Faq page ( finished )

--- page-resources.php
+++ page-resources.php
@@ -14,25 +14,42 @@ get_header();
 <div class='resource-page-container'>
 		<?php while ( have_posts() ) :the_post(); ?>
 
+		<div class="cstm-resource-title">
+			<?php
+				if(function_exists('get_field')){
+					if(get_field('resources_page_title')){
+						echo '<h2>';
+						echo the_field('resources_page_title');
+						echo '</h2>';
+					}
+				}
+			?>
+		</div>
+
 		<?php 
-			$resource_link = get_field('resource_link');
 			if (have_rows("resources")){
+				
 				echo '<div class="resource-content">';
 				while(have_rows("resources")){
+						
 					the_row();
+							$resource_link = get_sub_field('resource_link');
 						echo wp_get_attachment_image( get_sub_field('resource_image'), 'full' );
-						echo '<h2>';
+						echo '<div class="resource-description">';
+						echo '<h3>';
 							echo get_sub_field("resource_title");
-						echo '</h2>';
+						echo '</h3>';
 						echo '<p>';
 							echo get_sub_field("resource_content");
 						echo '</p>';
-						echo '<a href="esc_url( $resource_link )"> Read More ';
+						echo '<a href=" ' . esc_html($resource_link) . '"> Further reading';
 						echo '</a>';
+						echo '</div>';
 				echo '</div>';
 							}
 					}
-     ?>
+		 ?>
+		 
 </div>
 <!-- resource-page-container -->
     	<?php endwhile; // End of the loop.?>
