74602d2d4873a65191e58358b4b74f77c3862a81
Tim Wu <tim@longevitygraphics.com>
2020-08-22 08:44:32 -0700

    add search page

--- search.php
+++ search.php
@@ -1,50 +1,52 @@
 <?php
 /**
- * The template for displaying search results pages
+ * Main template file
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
+ * This page shows the search result.
  *
  * @package cedirectory
  */
 
 get_header();
+
 ?>
 
-	<main id="primary" class="site-main">
-<div class='search-result-container'>
-		<?php if ( have_posts() ) : ?>
-			<header style="padding-left: none;" class="page-header">
-				<h2 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'cedirectory' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h2>
-			</header><!-- .page-header -->
-		<?php
-			/* Start the Loop */
-			echo '<div class="search-result-content">';
-			while ( have_posts() ) :
-				the_post();
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-			echo '</div>';
-			endwhile;
-
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-	
-		endif;
+<?php if ( have_posts() ) : ?>
+	<div class="container py-3 mt-5">
+	<div class="page-header pb-4">
+		<h2 class="page-title">
+			<?php
+			/* translators: %s: search query. */
+			printf( esc_html__( 'Search Results for: %s', 'cedirectory' ), '<span>' . get_search_query() . '</span>' );
+			?>
+		</h2>
+	</div><!-- .page-header -->
+	<?php
+	while ( have_posts() ) :
+		the_post();
 		?>
+		<div class="pb-3">
+
+			<h2 class='mb-3'><a class="text-dark" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+			<p class='font-italic'><?php the_author(); ?></p>
+			<div class="article-content">
+				<p><?php the_excerpt(); ?></p>
+			</div>
+			<a class="d-inline-block mt-4 text-start text-secondary" href="<?php the_permalink(); ?>">Learn More</a>
+        </div>
+        <?php if (($wp_query->current_post +1) != ($wp_query->post_count)): ?>
+			<hr class="lg d-none d-md-block">
+		<?php endif; ?>
+
+	<?php endwhile; ?>
+	</div>
+<?php else: ?>
+        <div class='container py-5 text-center'>
+			<h2 class='mb-3'>Sorry, no search result for "<?php echo get_search_query(); ?>"</h2>
+            <div class='d-flex justify-content-center'>
+                <a class='btn btn-primary' href="/">Back to Home</a>
+            </div>
 		</div>
+<?php endif; ?>
 
-
-	</main><!-- #main -->
-
-<?php
-
-get_footer();
+<?php get_footer(); ?>
