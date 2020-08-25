6d224b14a506ade6a75fc3aa535b1e8479ecb3a7
Tim Wu <tim@longevitygraphics.com>
2020-08-24 17:31:47 -0700

    remove comment from single product page

--- single-product.php
+++ single-product.php
@@ -82,13 +82,6 @@ get_header();
 				<?php endif ?>
 				</div>
         </div>
-
-        <?php   // If comments are open or we have at least one comment, load up the comment template.
-                if ( comments_open() || get_comments_number() ) :
-                    comments_template();
-                endif;
-            endwhile; // End of the loop.
-		?>
         
     </main><!-- #main -->
     
