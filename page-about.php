a7ec645a232d3e2c5f181e61108204ff2dbf2659
Tim Wu <tim@longevitygraphics.com>
2020-09-09 15:36:55 -0700

    adjust about page

--- page-about.php
+++ page-about.php
@@ -54,7 +54,23 @@ get_header();
 						echo '</p>';
 					echo '</div>';
 			echo '</div>';
-		echo '</div>';
+		echo '</div>'; ?>
+			<div class="about-layer-three layer row">
+				<div class='col-lg-4'>
+					<?php echo wp_get_attachment_image( get_field('image_one', "option"), 'general' ); ?>
+					<p><?php echo the_field("layer_three_description_one", "option"); ?></p>
+				</div>
+				<div class='col-lg-4'>
+					<?php echo wp_get_attachment_image( get_field('image_two', "option"), 'general' ); ?>
+					<p><?php echo the_field("layer_three_description_two", "option"); ?></p>
+				</div>
+				<div class='col-lg-4'>
+					<?php echo wp_get_attachment_image( get_field('image_three', "option"), 'general' ); ?>
+					<p><?php echo the_field("layer_three_description_three", "option"); ?></p>
+				</div>
+			</div>
+
+			<?php
 
 			echo '<div class="about-layer-three layer">';
 					echo '<div class="layer-three-images">';
